93f504de8b73276e2717745fdc6320469c6f4f4f
Ahmedur Rahman Shovon <shovon.sylhet@gmail.com>
2020-09-12 13:10:02 +0600

    created first release

--- js/arsho.js
+++ js/arsho.js
@@ -7,7 +7,34 @@ $(document).ready(function(){
         "division": "÷"
     };
 
+    var bangla_to_english_mapper = {
+        '০': "0",
+        '১': "1",
+        '২': "2",
+        '৩': "3",
+        '৪': "4",
+        '৫': "5",
+        '৬': "6",
+        '৭': "7",
+        '৮': "8",
+        '৯': "9"
+    };
+
+    var english_to_bangla_mapper = {
+        "0": '০' ,
+        "1": '১',
+        "2": '২',
+        "3": '৩',
+        "4": '৪',
+        "5": '৫',
+        "6": '৬',
+        "7": '৭',
+        "8": '৮',
+        "9": '৯'
+    };
+
     var quiz_data = [];
+    var quiz_settings = {};
 
     function get_random_integer(min_number, max_number){
         return Math.floor(Math.random() * (max_number - min_number + 1) + min_number);
@@ -22,6 +49,28 @@ $(document).ready(function(){
         var random_index = Math.floor(Math.random() * valid_operators_keys.length);
         random_operator = valid_operators[valid_operators_keys[random_index]];
         return random_operator;
+    }
+
+    function get_converted_value(original_value, language_code){
+        mapper = "";
+        switch (language_code) {
+            case "en":
+            mapper = bangla_to_english_mapper;
+            break;
+            default:
+            mapper = english_to_bangla_mapper;
+        }
+        converted_value = [];
+        for(i = 0; i < original_value.length; i++){
+            character = original_value[i];
+            if(mapper.hasOwnProperty(character)){
+                converted_value.push(mapper[character]);
+            }
+            else{
+                converted_value.push(character);
+            }
+        }
+        return converted_value.join("");
     }
 
     function get_correct_answer(operand_1, operand_2, operator){
@@ -47,22 +96,26 @@ $(document).ready(function(){
 
     function get_a_quiz_row(question){
         return `<tr class="quiz_row bg-dark text-white text-center align-items-center">
-            <td>
-                ${question["operand_1"].toLocaleString('bn-BD')}
-            </td>
-            <td>
-                ${question["operator"]}
-            </td>
-            <td>
-                ${question["operand_2"].toLocaleString('bn-BD')}
-            </td>
-            <td>
-                =
-            </td>
-            <td>
-                <input id="${question["question_id"]}" class="answer_input form-control" type="text">
-            </td>
-            </tr>`;
+        <td>
+        ${question["operand_1"]}
+        </td>
+        <td>
+        ${question["operator"]}
+        </td>
+        <td>
+        ${question["operand_2"]}
+        </td>
+        <td>
+        =
+        </td>
+        <td>
+        <input id="${question["question_id"]}" class="answer_input form-control" type="text">
+        </td>
+        <td>
+        <button class="btn btn-light"><i class="fa fa-2x fa-share"></i></button>
+        </td>
+        </tr>`;
+        // fa-long-arrow-alt-right
     }
 
     function get_quiz_settings(){
@@ -89,7 +142,7 @@ $(document).ready(function(){
         };
     }
 
-    function get_quiz_data(quiz_settings){
+    function get_quiz_data(){
         data = [];
         operators = quiz_settings["type_of_questions"];
         max_number_operand_1 = quiz_settings["max_number_operand_1"];
@@ -103,6 +156,10 @@ $(document).ready(function(){
             question["correct_answer"] = get_correct_answer(operand_1, operand_2, operator);
             question["operand_1"] = operand_1;
             question["operand_2"] = operand_2;
+            if(quiz_settings["quiz_language"] === "bn"){
+                question["operand_1"] = operand_1.toLocaleString("bn-BD");
+                question["operand_2"] = operand_2.toLocaleString("bn-BD");
+            }
             question["operator"] = operator;
             question["question_id"] = i;
             data.push(question);
@@ -114,7 +171,7 @@ $(document).ready(function(){
         quiz_settings = get_quiz_settings();
         $("#quiz_wrapper").removeClass("invisible");
         $("#quiz_settings_box").collapse('hide');
-        quiz_data = get_quiz_data(quiz_settings);
+        quiz_data = get_quiz_data();
         $("#quiz_rows").html("");
         for(i = 0; i < quiz_data.length; i++){
             $("#quiz_rows").append(get_a_quiz_row(quiz_data[i]));
@@ -122,7 +179,7 @@ $(document).ready(function(){
     });
 
     $("body").on("focusout", "input.answer_input", function(event){
-        current_value = $(this).val();
+        current_value = get_converted_value($(this).val(), "en");
         question_id = parseInt($(this).attr("id"));
         correct_anwer = quiz_data[question_id]["correct_answer"].toString();
         if(current_value === correct_anwer){
@@ -138,10 +195,11 @@ $(document).ready(function(){
     });
 
     $("body").on("keyup", "input.answer_input", function(event){
-        current_value = parseInt($(this).val());
-        bangla_value = current_value.toLocaleString('bn-BD');
-        console.log(current_value, bangla_value);
-        $(this).html(bangla_value);
+        current_value = get_converted_value($(this).val(), "en");
+        if(quiz_settings["quiz_language"] === "bn"){
+            bangla_value = get_converted_value(current_value, "bn");
+            $(this).val(bangla_value);
+        }
     });
 
 
--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
 ## Kids Math Library
 
 ![alt Kids Math Demo](screenshots/kids_math_demo.png)
+
+### Features
+- Math quiz generator for two operands and an operator.
+- Quiz can be generated for both Bangla and English math.
+- Maximum number for both operands can be controlled.
+- Multiple types of operators can be chosen.
