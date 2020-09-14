0b7d2e14ce1b03f661eed3f8350d689aa97cacd9
Ahmedur Rahman Shovon <shovon.sylhet@gmail.com>
2020-09-14 16:50:58 +0600

    added responsive font size for table

--- js/arsho.js
+++ js/arsho.js
@@ -180,15 +180,24 @@ $(document).ready(function(){
         for(i = 0; i < quiz_data.length; i++){
             $("#quiz_rows").append(get_a_quiz_row(quiz_data[i]));
         }
-        $("#total_questions_value").html(quiz_settings["number_of_questions"]);
+        $("#correct_answers_title").html("Correct Answer");
+        $("#wrong_answers_title").html("Wrong Answer");
         $("#correct_answers_value").html(0);
         $("#wrong_answers_value").html(0);
+        if(quiz_settings["quiz_language"] === "bn"){
+            $("#correct_answers_title").html("সঠিক উত্তর");
+            $("#wrong_answers_title").html("ভুল উত্তর");
+            $("#correct_answers_value").html(get_converted_value("0", "bn"));
+            $("#wrong_answers_value").html(get_converted_value("0", "bn"));
+        }
     });
 
     $("body").on("focusout", "input.answer_input", function(event){
         current_value = get_converted_value($(this).val(), "en");
         question_id = parseInt($(this).attr("id"));
         correct_anwer = quiz_data[question_id]["correct_answer"].toString();
+        total_correct_old = $(".quiz_row.bg-success").length;
+        total_wrong_old = $(".quiz_row.bg-danger").length;
         if(current_value === correct_anwer){
             $(this).closest(".quiz_row").removeClass("bg-dark");
             $(this).closest(".quiz_row").removeClass("bg-danger");
@@ -200,8 +209,22 @@ $(document).ready(function(){
             $(this).closest(".quiz_row").removeClass("bg-success");
             $(this).closest(".quiz_row").addClass("bg-danger");
         }
-        $("#correct_answers_value").html($(".quiz_row.bg-success").length);
-        $("#wrong_answers_value").html($(".quiz_row.bg-danger").length);
+        total_correct_new = $(".quiz_row.bg-success").length;
+        total_wrong_new = $(".quiz_row.bg-danger").length;
+        if(total_correct_new !== total_correct_old){
+            $("#correct_answers_card").fadeOut();
+            $("#correct_answers_value").html(total_correct_new);
+            $("#correct_answers_card").fadeIn();
+        }
+        if(total_wrong_new !== total_wrong_old){
+            $("#wrong_answers_card").fadeOut();
+            $("#wrong_answers_value").html(total_wrong_new);
+            $("#wrong_answers_card").fadeIn();
+        }
+        if(quiz_settings["quiz_language"] === "bn"){
+            $("#correct_answers_value").html(get_converted_value(total_correct_new.toString(), "bn"));
+            $("#wrong_answers_value").html(get_converted_value(total_wrong_new.toString(), "bn"));
+        }
     });
 
     $("body").on("keyup", "input.answer_input", function(event){
